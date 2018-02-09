1c99b7a644bccba68e9c5aa8385ff3ab0aa682e9
Christina Nita <Christina Nita>
2018-02-09 19:45:42 +0000

    Enemy spawning implemented

---
+++ Project/Revelation of Worlds/Assets/Scripts/Enemy/EnemyManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+    //public PlayerHealth playerHealth;
+    public GameObject enemy;
+    public float spawnTime = 3f; // 3 seconds spawn time
+    public Transform[] spawnPoints;
+
+	// Use this for initialization
+	void Start () {
+        // repeats Spawn every 3 seconds
+        // InvokeRepeating (function, wait before spawning, wait before repeating)
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+	}
+	
+	/* Update is called once per frame
+	void Update () {
+		
+	}*/
+
+    void Spawn()
+    {
+        // if player health is less than 0 nobody is spawned
+        /*if(playerHealth.currentHealth <= 0f)
+        {
+            return;
+        }*/
+
+        // spawnPointIndex represents a point in the array
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        // create an instance of an object
+        // Instantiate (what, where, rotation)
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+}
