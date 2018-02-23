c76a3180f0d7e43657ce6375baae0a937071694b
kamilam1987 <kamilam1987@gmail.com>
2018-02-23 19:57:22 +0000

    added main menu

---
+++ Project/Revelation of Worlds/Assets/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Author:Kamila Michel
+//Source codeadapted from: https://www.youtube.com/watch?v=zc8ac_qUXQY&t=605s
+public class MainMenu : MonoBehaviour {
+
+    //Medhod created for Play option from main menu
+    public void PlayGame() {
+
+        //Loads the next level(next Scene)
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //Mehod created for Quit option from main menu
+    public void QuitGame()
+    {
+        //Prints Quit to the console, checks if the quit button is working
+        Debug.Log("Quit");
+        //Closes the program
+        Application.Quit();
+    }
+
+}
+
