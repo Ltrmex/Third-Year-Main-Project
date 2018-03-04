fd16a24a11ee0894ad6a15be50cee27c2078026d
Ltrmex <maciekmajchrzak23@wp.pl>
2018-03-04 15:15:56 +0000

    button functionality added

---
+++ Revelation of Worlds/Assets/Scripts/Character Stats/PanelHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelHandler : MonoBehaviour {
+
+    public GameObject menu; // Assign in inspector
+    private bool isShowing;
+
+    void Update()
+    {
+        if (Input.GetKeyDown("r"))
+        {
+            isShowing = !isShowing;
+            menu.SetActive(isShowing);
+        }
+    }
+}
