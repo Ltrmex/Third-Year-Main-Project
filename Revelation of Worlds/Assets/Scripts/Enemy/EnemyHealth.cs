4f89be8b647006f9290eaeb3a8977840ea259d2f
Ltrmex <maciekmajchrzak23@wp.pl>
2018-03-12 19:32:41 +0000

    error fixing

--- Revelation of Worlds/Assets/Scripts/Enemy/EnemyHealth.cs
+++ Revelation of Worlds/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -13,7 +13,7 @@ public class EnemyHealth : MonoBehaviour
     //Animator anim;                              // Reference to the animator.
     //AudioSource enemyAudio;                     // Reference to the audio source.
     //ParticleSystem hitParticles;                // Reference to the particle system that plays when the enemy is damaged.
-    CapsuleCollider capsuleCollider;            // Reference to the capsule collider.
+    BoxCollider capsuleCollider;            // Reference to the capsule collider.
     bool isDead;                                // Whether the enemy is dead.
     bool isSinking;                             // Whether the enemy has started sinking through the floor.
     
@@ -25,7 +25,7 @@ public class EnemyHealth : MonoBehaviour
         //anim = GetComponent<Animator>();
         //enemyAudio = GetComponent<AudioSource>();
         //hitParticles = GetComponentInChildren<ParticleSystem>();
-        capsuleCollider = GetComponent<CapsuleCollider>();
+        capsuleCollider = GetComponent<BoxCollider>();
 
         // Setting the current health when the enemy first spawns.
         currentHealth = startingHealth;
