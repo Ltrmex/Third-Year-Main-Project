4c7587e000688adedb027ec002693a98ca38aa1d
kamilam1987 <kamilam1987@gmail.com>
2018-03-17 16:41:07 +0000

    Menu added to game over

---
+++ Revelation of Worlds/Assets/GameOver.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class GameOver : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject mainMenuHolder;
+    public GameObject mainOnGameOver;
+
+    public void Play()
+    {
+        //SceneManager.LoadScene("Level01");
+        //Loads the next level(next Scene)
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        //Prints Quit to the console, checks if the quit button is working
+        //Debug.Log("Quit");
+        //Closes the program
+        Application.Quit();
+    }
+ 
+    public void MainMenuGameOver()
+    {
+        mainOnGameOver.SetActive(true);
+        mainMenuHolder.SetActive(false);
+    }
+   
+}
