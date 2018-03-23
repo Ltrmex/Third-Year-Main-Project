8111c7309f9807b6b15b431b1aa08fdf7de06cf7
Ltrmex <maciekmajchrzak23@wp.pl>
2018-03-23 22:25:48 +0000

    on level change enemy health is increased by 5 percent

--- Revelation of Worlds/Assets/Scripts/Behaviour/DifficultyRaise.cs
+++ Revelation of Worlds/Assets/Scripts/Behaviour/DifficultyRaise.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class DifficultyRaise : MonoBehaviour {
     public static bool isLevelUp;
     MapGenerator generate;
+    public EnemyHealth enemy;
 
     // Use this for initialization
     void Start () {
         generate = FindObjectOfType<MapGenerator>();
+        enemy.startingHealth = 100;
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (isLevelUp == true) {
             generate.GenerateMap();
+            enemy.startingHealth += ((int)(enemy.startingHealth * 0.1));
             isLevelUp = false;
         }   //  Update()
 	}
